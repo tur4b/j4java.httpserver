99cc6b8cd0f8fb6cabe1feb1f9ac1aabd26f22f6
turab <eybaliyev98@gmail.com>
2025-08-20 19:37:43 +0400

    feature: adding request param functionality, many request params can be used

--- src/main/java/com/j4httpserver/config/HttpServerAutoConfiguration.java
+++ src/main/java/com/j4httpserver/config/HttpServerAutoConfiguration.java
@@ -1,15 +1,12 @@
 package com.j4httpserver.config;
 
-import com.j4httpserver.annotation.RequestBody;
+import com.j4httpserver.annotation.*;
 import com.j4httpserver.enums.HttpMethod;
 import com.j4httpserver.model.ErrorResponse;
 import com.j4httpserver.util.ClassScanner;
 import com.j4httpserver.util.JsonUtil;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
-import com.j4httpserver.annotation.EnableHttpServer;
-import com.j4httpserver.annotation.HttpEndpoint;
-import com.j4httpserver.annotation.HttpServerExchange;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,6 +16,8 @@ import java.lang.reflect.Parameter;
 import java.lang.reflect.Type;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.*;
 import java.util.logging.Logger;
@@ -67,6 +66,8 @@ public final class HttpServerAutoConfiguration {
         httpServer.start();
     }
 
+    // ******************************** helper methods *********************************
+
     private static void createHttpServerContexts(HttpServerExchange httpServerExchange,
                                                  HttpServer httpServer,
                                                  Object httpServerExchangeInstance,
@@ -90,7 +91,6 @@ public final class HttpServerAutoConfiguration {
                     }
 
                     log.info(">> exchange handler handles: " + exchange.getRequestMethod() + " " + exchange.getRequestURI());
-
 
                     final Object[] args = getMethodArguments(method, exchange);
 
@@ -119,20 +119,35 @@ public final class HttpServerAutoConfiguration {
         for(int i = 0; i < method.getParameterCount(); i++) {
             Parameter parameter = parameters[i];
             Type parameterizedType = parameter.getParameterizedType();
-            log.info(">> parameter: " + parameter.getName() + " - " + parameterizedType.getTypeName());
+            String parameterName = parameter.getName();
+            log.info(">> parameter: " + parameterName + " - " + parameterizedType.getTypeName());
 
             RequestBody requestBodyAnnotation = parameter.getAnnotation(RequestBody.class);
+            RequestParam requestParamAnnotation = parameter.getAnnotation(RequestParam.class);
+            System.out.println(">> requestParamAnnotation: " + requestParamAnnotation);
 
             if("com.sun.net.httpserver.HttpExchange".equalsIgnoreCase(parameterizedType.getTypeName())) {
                 args[i] = exchange;
-            } else if(requestBodyAnnotation != null) {
+                continue;
+            }
+            if(requestBodyAnnotation != null) {
                 InputStream requestInputStream = exchange.getRequestBody();
                 Object requestBody = JsonUtil.readAs(requestInputStream, parameterizedType);
                 args[i] = requestBody;
+                continue;
             }
-            else {
-                args[i] = parameter;
+            if(requestParamAnnotation != null) {
+                boolean required = requestParamAnnotation.required();
+//                String requestParamName = requestParamAnnotation.name().isEmpty() ? parameterName : requestParamAnnotation.name();
+                Object arg = getRequestParamByName(exchange, requestParamAnnotation.name(), parameterizedType);
+
+                if(required && arg == null) {
+                    throw new RuntimeException("Required parameter " + requestParamAnnotation.name() + " not found");
+                }
+                args[i] = arg;
+                continue;
             }
+            args[i] = parameter;
         }
         return args;
     }
@@ -144,5 +159,23 @@ public final class HttpServerAutoConfiguration {
         exchange.sendResponseHeaders(errorResponse.statusCode(), responseAsJsonBytes.length); // method not allowed
         responseOutputStream.write(responseAsJsonBytes);
         responseOutputStream.flush();
+    }
+
+    private static Map<String, String> getRequestParams(HttpExchange httpExchange) {
+        Map<String, String> requestParams = new HashMap<>();
+        String query = httpExchange.getRequestURI().getQuery();
+        if(query != null && !(query = query.trim()).isEmpty()) {
+            String[] parameters = query.split("&");
+            for(String parameter : parameters) {
+                String[] keyValue = parameter.split("=");
+                requestParams.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return requestParams;
+    }
+
+    private static Object getRequestParamByName(HttpExchange httpExchange, String paramName, Type paramType) {
+       String value = getRequestParams(httpExchange).get(paramName);
+       return JsonUtil.convertValue(value, paramType);
     }
 }
